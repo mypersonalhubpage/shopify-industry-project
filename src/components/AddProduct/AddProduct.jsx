53aed21d5c833ca44ab684f741530de91fc87dc2
Chris Nguyen <chrisngtutor@gmail.com>
2022-03-24 22:15:39 -0700

    fixed explore button and its route

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,22 +1,29 @@
 import "./AddProduct.scss";
 import tag from "../../assets/logo/tag.png";
+import { Link } from "react-router-dom";
 function AddProduct() {
   return (
     <>
-      <div className="img-container">
-        <img src={tag} className="tag" alt="tag image" />
-      </div>
+      <div className="test-container">
+        <div className="img-container">
+          <img src={tag} className="tag" alt="tag image" />
+        </div>
 
-      <p className="add-subheader">Add your first product</p>
-      <p className="add-description">
-        Add physical items, digital downloads, services, or anything else you
-        dream up.
-      </p>
-      <button className="btn">Add product</button>
-      <p className="juststart-subheader">
-        Just getting started? Let us help you find the right fit!
-      </p>
-      <button className="getstart-btn btn">Explore</button>
+        <p className="add-subheader">Add your first product</p>
+        <p className="add-description">
+          Add physical items, digital downloads, services, or anything else you
+          dream up.
+        </p>
+        <button className="btn">Add product</button>
+        <p className="juststart-subheader">
+          Just getting started? Let us help you find the right fit!
+        </p>
+        <div className="button-container">
+          <Link to="/form" className="getstart-btn btn">
+            Explore
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
