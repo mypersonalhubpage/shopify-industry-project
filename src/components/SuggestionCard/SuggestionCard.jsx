68d038eea1f4c64bedae9527859598167f8bc674
Chris Nguyen <chrisngtutor@gmail.com>
2022-03-25 10:32:00 -0700

    added new feature to suggestion page

--- src/components/SuggestionCard/SuggestionCard.jsx
+++ src/components/SuggestionCard/SuggestionCard.jsx
@@ -1,23 +1,37 @@
 import "./SuggestionCard.scss";
 import addIcon from "../../assets/icons/add.svg";
-
+import { useState } from "react";
 function SuggestionCard(props) {
-	console.log(props);
-	const { name, price, img } = props.product;
+  console.log(props);
+  const { name, price, img } = props.product;
+  const [added, setAdded] = useState(false);
+  const addHandler = (e) => {
+    if (added) {
+      setAdded(false);
+    } else {
+      setAdded(true);
+    }
+  };
 
-	return (
-		<div className='suggestion-card'>
-			<div className='suggestion-card__wrapper'>
-				<p className='suggestion-card__name'>{name}</p>
-				<p className='suggestion-card__price'>{price}</p>
-				<img className='suggestion-card__img' src={img} alt='product' />
-				<div className='suggestion-card__add-wrapper'>
-					<p className='suggestion-card__add'>Add Product</p>
-					<img className='suggestion-card__add-btn' src={addIcon} alt='plus' />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="suggestion-card">
+      <div className="suggestion-card__wrapper">
+        <p className="suggestion-card__name">{name}</p>
+        <p className="suggestion-card__price">{price}</p>
+        <img className="suggestion-card__img" src={img} alt="product" />
+        <div className="suggestion-card__add-wrapper">
+          <p className="suggestion-card__add">Add Product</p>
+          <img
+            onClick={addHandler}
+            className="suggestion-card__add-btn"
+            src={addIcon}
+            alt="plus"
+          />
+        </div>
+        <div className={`added ${added ? "added--true" : ""}`}>Added</div>
+      </div>
+    </div>
+  );
 }
 
 export default SuggestionCard;
