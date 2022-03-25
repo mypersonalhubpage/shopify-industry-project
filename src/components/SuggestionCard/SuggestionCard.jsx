7503e73659e2da192afb7efb84acdb24d347f448
Pedro Chang <pedrochang93@gmail.com>
2022-03-25 11:05:48 -0400

    cards added

--- src/components/SuggestionCard/SuggestionCard.jsx
+++ src/components/SuggestionCard/SuggestionCard.jsx
@@ -1,1 +1,23 @@
 import "./SuggestionCard.scss";
+import addIcon from "../../assets/icons/add.svg";
+
+function SuggestionCard(props) {
+	console.log(props);
+	const { name, price, img } = props.product;
+
+	return (
+		<div className='suggestion-card'>
+			<div className='suggestion-card__wrapper'>
+				<p className='suggestion-card__name'>{name}</p>
+				<p className='suggestion-card__price'>{price}</p>
+				<img className='suggestion-card__img' src={img} alt='product' />
+				<div className='suggestion-card__add-wrapper'>
+					<p className='suggestion-card__add'>Add Product</p>
+					<img className='suggestion-card__add-btn' src={addIcon} alt='plus' />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SuggestionCard;
--- src/pages/Suggestion/Suggestion.jsx
+++ src/pages/Suggestion/Suggestion.jsx
@@ -5,6 +5,9 @@ import powerbeats from "../../assets/img/powerbeats-pro.svg";
 import mouse from "../../assets/img/mouse.svg";
 import keyboard from "../../assets/img/keyboard.svg";
 import watch from "../../assets/img/watch.svg";
+import SuggestionCard from "../../components/SuggestionCard/SuggestionCard";
+import detailBall1 from "../../assets/img/detail-ball1.svg";
+import downArrow from "../../assets/icons/down-arrow.svg";
 
 function Suggestion() {
 	const products = [
@@ -32,6 +35,7 @@ function Suggestion() {
 
 	return (
 		<section className='suggestion'>
+			<img className='suggestion__detail1' src={detailBall1} alt='detail 1' />
 			<div className='suggestion__wrapper'>
 				<div className='suggestion__top-wrapper'>
 					<GoBack color='white' />
@@ -41,6 +45,18 @@ function Suggestion() {
 				<p className='suggestion__text'>
 					we recommend you start selling the following products:
 				</p>
+				<div className='suggestion__cards'>
+					{products.map((product, i) => (
+						<SuggestionCard key={i} product={product} />
+					))}
+				</div>
+				<div className='suggestion__scroll-text-wrapper'>
+					<p className='suggestion__scroll-text suggestion__scroll-text--margin'>
+						Need financial assistance?
+					</p>
+					<p className='suggestion__scroll-text'>Explore Shopify Capital</p>
+					<img src={downArrow} alt='' />
+				</div>
 			</div>
 		</section>
 	);
