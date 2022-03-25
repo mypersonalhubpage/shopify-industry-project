db01c5ed54aa7b836c3554c1971eb4d18ecbac26
Pedro Chang <pedrochang93@gmail.com>
2022-03-25 08:21:45 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Switch, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import FormPage from "./pages/FormPage/FormPage";
 import Suggestion from "./pages/Suggestion/Suggestion";
+import Footer from "./components/Footer/Footer";
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/home" component={Main} />
-        <Route path="/form" component={FormPage} />
-        <Route path="/suggestion" component={Suggestion} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Switch>
+				<Route path='/' exact component={Main} />
+				<Route path='/home' component={Main} />
+				<Route path='/form' component={FormPage} />
+				<Route path='/suggestion' component={Suggestion} />
+			</Switch>
+			<Route path='/:path' component={Footer} />
+		</div>
+	);
 }
 
 export default App;
